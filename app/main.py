88b17b9b1442d3110ea23eb57754167258bb5d9c
Kritika Ramola <140999579+kriti1456@users.noreply.github.com>
2026-09-21 03:32:31 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",          # Vite dev server
-        "https://northbridge-ai-liart.vercel.app",
+        "https://rag-frontend-project.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
